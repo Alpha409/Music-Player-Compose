674ada50dfa72c4b9207b08d3ffaff265dcd8696
Alpha409 <awais.umer409@gmail.com>
2025-12-12 21:08:22 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/splash/SplashScreen.kt
+++ app/src/main/java/com/example/musicplayercompose/splash/SplashScreen.kt
@@ -3,14 +3,25 @@ package com.example.musicplayercompose.splash
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 
 @Composable
 fun SplashScreen (navController: NavController) {
+
+
     Button(onClick = {
         navController.navigate("profile")
     }) {
         Text("Go to Profile")
     }
+}
+
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen(navController = NavController(LocalContext.current))
 }
