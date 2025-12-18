3b665d29060464b451c7b1d3e5fc6447ec75f71d
Alpha409 <awais.umer409@gmail.com>
2025-12-18 10:00:51 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/presentation/mainscreen/MainScreen.kt
+++ app/src/main/java/com/example/musicplayercompose/presentation/mainscreen/MainScreen.kt
@@ -2,12 +2,15 @@ package com.example.musicplayercompose.presentation.mainscreen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
@@ -15,12 +18,26 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.musicplayercompose.common.bottomnav.BottomNavItem
 
+
+
+val bottomBarRoutes = listOf(
+    BottomNavItem.Home.route,
+    BottomNavItem.MyMusic.route,
+    BottomNavItem.Favorite.route
+)
+
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen() {
     val navController = rememberNavController()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold(
         bottomBar = {
-            BottomBar(navController)
+            if (currentRoute in bottomBarRoutes) {
+                BottomBar(navController)
+            }
         }
     ) { padding ->
         Box(modifier = Modifier.padding(padding)) {
@@ -28,16 +45,20 @@ fun MainScreen(navController: NavController) {
         }
     }
 }
+
 @Composable
 fun BottomBar(navController: NavController) {
+
     val items = listOf(
         BottomNavItem.Home,
         BottomNavItem.MyMusic,
         BottomNavItem.Favorite
     )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     NavigationBar {
-        val currentRoute = navController
-            .currentBackStackEntryAsState().value?.destination?.route
         items.forEach { item ->
             NavigationBarItem(
                 selected = currentRoute == item.route,
@@ -50,7 +71,12 @@ fun BottomBar(navController: NavController) {
                         restoreState = true
                     }
                 },
-                icon = { Icon(item.icon, contentDescription = item.label) },
+                icon = {
+                    Icon(
+                        painter = painterResource(item.icon),
+                        contentDescription = item.label
+                    )
+                },
                 label = { Text(item.label) }
             )
         }
