777bbe5783eb6e9908a9df61929dfdc1abc1f3f8
Alpha409 <awais.umer409@gmail.com>
2025-12-19 20:40:41 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/common/bottomnav/BottomNavItem.kt
+++ app/src/main/java/com/example/musicplayercompose/common/bottomnav/BottomNavItem.kt
@@ -5,11 +5,10 @@ import com.example.musicplayercompose.R
 
 
 sealed class BottomNavItem(
-    val route: String,
-    val icon: Int,
-    val label: String
+    val route: String, val icon: Int, val label: String
 ) {
-    object Home : BottomNavItem("home", R.drawable.home, "Home")
+
     object MyMusic : BottomNavItem("myMusic", R.drawable.music, "MyMusic")
+    object Home : BottomNavItem("home", R.drawable.home, "Home")
     object Favorite : BottomNavItem("fav", R.drawable.fav, "Favorite")
 }
