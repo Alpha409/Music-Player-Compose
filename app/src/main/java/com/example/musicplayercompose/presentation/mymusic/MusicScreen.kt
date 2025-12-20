f18e966ba132f65b8f398e18f1f29d81475bb092
Alpha409 <awais.umer409@gmail.com>
2025-12-20 20:02:18 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/presentation/mymusic/MusicScreen.kt
+++ app/src/main/java/com/example/musicplayercompose/presentation/mymusic/MusicScreen.kt
@@ -2,7 +2,6 @@ package com.example.musicplayercompose.presentation.mymusic
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -12,9 +11,19 @@ import androidx.navigation.NavController
 @Composable
 fun MusicScreen(navController: NavController) {
     Column(modifier = Modifier.fillMaxSize()){
-
+        MusicItem(navController)
     }
 }
+
+
+
+@Composable
+fun MusicItem(navController: NavController) {
+
+
+
+}
+
 
 @Preview
 @Composable
