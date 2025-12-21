0d1606a8b34daca38570b5616a2c7e0498216314
Alpha409 <awais.umer409@gmail.com>
2025-12-21 18:55:35 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/presentation/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/musicplayercompose/presentation/favorite/FavoriteScreen.kt
@@ -1,10 +1,30 @@
 package com.example.musicplayercompose.presentation.favorite
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.musicplayercompose.R
+import com.example.musicplayercompose.presentation.mymusic.MusicItem
 
 @Composable
 fun FavoriteScreen(navController: NavController) {
-    Text(text = "Favorite Screen" )
+    Column(modifier = Modifier.fillMaxSize()) {
+        MusicItem(navController)
+    }
+}
+
+
+
+@Preview
+@Composable
+fun previewMusicScreen() {
+    FavoriteScreen(navController = NavController(LocalContext.current))
 }
