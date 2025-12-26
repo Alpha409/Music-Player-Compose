e448bfb05caf1ee4def6d00ccbc1880a36cddca8
Alpha409 <awais.umer409@gmail.com>
2025-12-26 21:13:42 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/common/extensionFunctions/ViewsExtensionF.kt
+++ app/src/main/java/com/example/musicplayercompose/common/extensionFunctions/ViewsExtensionF.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 object ViewsExtensionF {
+
     fun View.hideKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
