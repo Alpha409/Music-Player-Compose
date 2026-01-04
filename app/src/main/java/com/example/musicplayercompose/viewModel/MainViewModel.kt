088ab70a2c7376b3418b44e37c8885fbd625d89e
Alpha409 <awais.umer409@gmail.com>
2026-01-04 22:37:16 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/musicplayercompose/viewModel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.musicplayercompose.domain.models.Mp3FilesDataClass
 import com.example.musicplayercompose.domain.repository.FavSongsRepo
 import com.example.musicplayercompose.domain.repository.GetMusicLocalRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
