09f3c404fe8792579df66a22286d95945ae8304d
Alpha409 <awais.umer409@gmail.com>
2026-01-17 21:40:15 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/data/datasource/db/FavDatabase.kt
+++ app/src/main/java/com/example/musicplayercompose/data/datasource/db/FavDatabase.kt
@@ -6,9 +6,7 @@ import androidx.room.TypeConverters
 import com.example.musicplayercompose.common.utils.BitmapConverters
 import com.example.musicplayercompose.domain.models.Mp3FilesDataClass
 
-@Database(
-    entities = [Mp3FilesDataClass::class], version = 3, exportSchema = false
-)
+@Database(entities = [Mp3FilesDataClass::class], version = 3, exportSchema = false)
 @TypeConverters(BitmapConverters::class)
 abstract class FavDatabase : RoomDatabase() {
     abstract fun favDao(): FavSongsDao
