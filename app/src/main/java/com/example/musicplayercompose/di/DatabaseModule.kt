74e0ed6eb187c453570f709f43f00c8dbe903e03
Alpha409 <awais.umer409@gmail.com>
2026-01-30 21:57:40 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/di/DatabaseModule.kt
+++ app/src/main/java/com/example/musicplayercompose/di/DatabaseModule.kt
@@ -19,8 +19,7 @@ class DatabaseModule {
     fun provideFavDatabase(
         @ApplicationContext context: Context
     ): FavDatabase {
-        return Room.databaseBuilder(
-            context.applicationContext, FavDatabase::class.java, "my_fav_db"
+        return Room.databaseBuilder(context.applicationContext, FavDatabase::class.java, "my_fav_db"
         ).fallbackToDestructiveMigration().build()
     }
 
