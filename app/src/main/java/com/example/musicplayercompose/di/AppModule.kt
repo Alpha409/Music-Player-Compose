b06cad3d2447729c4fb20c9816233034bd367be9
Alpha409 <awais.umer409@gmail.com>
2026-02-02 22:11:35 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/di/AppModule.kt
+++ app/src/main/java/com/example/musicplayercompose/di/AppModule.kt
@@ -34,7 +34,6 @@ class AppModule {
     fun providesGetMusicRepo(dataSource: LocalDataSource): GetMusicLocalRepo {
         return GetMusicLocalRepoImpl(dataSource)
     }
-
     @Provides
     @Singleton
     fun providesFavRepo(favSource: FavSongsDataSource): FavSongsRepo {
