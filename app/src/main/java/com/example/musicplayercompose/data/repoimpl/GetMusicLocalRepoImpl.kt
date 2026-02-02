3288ced6a835e39c17496c83298526efb5d77621
Alpha409 <awais.umer409@gmail.com>
2026-02-02 22:09:46 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/data/repoimpl/GetMusicLocalRepoImpl.kt
+++ app/src/main/java/com/example/musicplayercompose/data/repoimpl/GetMusicLocalRepoImpl.kt
@@ -6,8 +6,7 @@ import com.example.musicplayercompose.domain.repository.GetMusicLocalRepo
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetMusicLocalRepoImpl @Inject constructor(var localSource: LocalDataSource) :
-    GetMusicLocalRepo {
+class GetMusicLocalRepoImpl @Inject constructor(var localSource: LocalDataSource) : GetMusicLocalRepo {
     override fun getMp3LocalFiles(): Flow<List<Mp3FilesDataClass>> {
         return localSource.getMp3Files()
     }
