94ced92c940a8428faee1496155aa56e6058c1f6
Alpha409 <awais.umer409@gmail.com>
2026-03-25 22:26:08 +0500

    First Commit

--- app/src/main/java/com/example/musicplayercompose/data/datasource/db/FavDatabase.kt
+++ app/src/main/java/com/example/musicplayercompose/data/datasource/db/FavDatabase.kt
@@ -3,7 +3,6 @@ package com.example.musicplayercompose.data.datasource.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.musicplayercompose.common.utils.BitmapConverters
 import com.example.musicplayercompose.domain.models.Mp3FilesDataClass
 
 @Database(entities = [Mp3FilesDataClass::class], version = 3, exportSchema = false)
